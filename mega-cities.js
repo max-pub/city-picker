7039646c6dd6670762337a33684fcccd93e03238
Max Fechner <max.fechner@gmail.com>
2016-04-15 12:32:45 +0200

    updated star-system

---
+++ mega-cities.js
@@ -0,0 +1,83 @@
+MegaCities = [{
+		"city": "Shanghai",
+		"country": "China",
+		"population": 36,
+		"lat": 31.20,
+		"lon": 121.50
+	}, {
+		"city": "New York",
+		"country": "United States",
+		"population": 24,
+		"lat": 40.71,
+		"lon": -74.00
+	}, {
+		"city": "Bangkok",
+		"country": "Thailand",
+		"population": 15,
+		"lat": 13.76,
+		"lon": 100.50
+	}, {
+		"city": "Tokyo",
+		"country": "Japan",
+		"population": 39,
+		"lat": 35.68,
+		"lon": 139.68
+	}, {
+		"city": "Jakarta",
+		"country": "Indonesia",
+		"population": 32,
+		"lat": -6.17,
+		"lon": 106.82
+	}, {
+		"city": "Seoul",
+		"country": "South Korea",
+		"population": 26,
+		"lat": 37.57,
+		"lon": 126.96
+	}, {
+		"city": "Delhi",
+		"country": "India",
+		"population": 22,
+		"lat": 28.61,
+		"lon": 77.21
+	}, {
+		"city": "Sao Paulo",
+		"country": "Brazil",
+		"population": 22,
+		"lat": -23.55,
+		"lon": -46.63
+	}, {
+		"city": "Lagos",
+		"country": "Nigeria",
+		"population": 21,
+		"lat": 6.52,
+		"lon": 3.38
+	}, {
+		"city": "London",
+		"country": "England",
+		"population": 14,
+		"lat": 51.51,
+		"lon": -0.13
+	}, {
+		"city": "Paris",
+		"country": "France",
+		"population": 12,
+		"lat": 48.86,
+		"lon": 2.35
+	}, {
+		"city": "Moscow",
+		"country": "Russia",
+		"population": 17,
+		"lat": 55.76,
+		"lon": 37.62
+	}, {
+		"city": "Cairo",
+		"country": "Egypt",
+		"population": 19,
+		"lat": 30.04,
+		"lon": 31.24
+	}
+
+
+
+]
