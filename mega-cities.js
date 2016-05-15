2987a17d46c7ab1d16b1a017c292214fc2b13b8a
Max Fechner <max.fechner@gmail.com>
2016-05-15 11:51:57 +0200

    cities

--- mega-cities.js
+++ mega-cities.js
@@ -2,14 +2,14 @@ MegaCities = [{
 		"city": "Shanghai",
 		"country": "China",
 		"population": 36,
-		"lat": 31.20,
-		"lon": 121.50
+		"lat": 31.23,
+		"lon": 121.47
 	}, {
 		"city": "New York",
 		"country": "United States",
 		"population": 24,
 		"lat": 40.71,
-		"lon": -74.00
+		"lon": -74.01
 	}, {
 		"city": "Bangkok",
 		"country": "Thailand",
@@ -17,23 +17,23 @@ MegaCities = [{
 		"lat": 13.76,
 		"lon": 100.50
 	}, {
-		"city": "Tokyo",
-		"country": "Japan",
-		"population": 39,
-		"lat": 35.68,
-		"lon": 139.68
-	}, {
-		"city": "Jakarta",
-		"country": "Indonesia",
-		"population": 32,
-		"lat": -6.17,
-		"lon": 106.82
-	}, {
+	// 	"city": "Tokyo",
+	// 	"country": "Japan",
+	// 	"population": 39,
+	// 	"lat": 35.68,
+	// 	"lon": 139.68
+	// }, {
+	// 	"city": "Jakarta",
+	// 	"country": "Indonesia",
+	// 	"population": 32,
+	// 	"lat": -6.17,
+	// 	"lon": 106.82
+	// }, {
 		"city": "Seoul",
 		"country": "South Korea",
 		"population": 26,
 		"lat": 37.57,
-		"lon": 126.96
+		"lon": 126.98
 	}, {
 		"city": "Delhi",
 		"country": "India",
@@ -76,7 +76,86 @@ MegaCities = [{
 		"population": 19,
 		"lat": 30.04,
 		"lon": 31.24
+	}, {
+		"city": "Mexico City",
+		"country": "Mexico",
+		"population": 22,
+		"lat": 19.43,
+		"lon": -99.13
+	}, {
+		"city": "Kyoto",
+		"country": "Japan",
+		"population": 20,
+		"lat": 35.01,
+		"lon": 135.77
+	}, {
+		"city": "Hong Kong",
+		"country": "China",
+		"population": 20,
+		"lat": 22.28,
+		"lon": 114.17
+	}, {
+		"city": "Kinshasa",
+		"country": "Congo",
+		"population": 12,
+		"lat": -4.44,
+		"lon": 15.27
+	}, {
+		"city": "Manila",
+		"country": "Philippines",
+		"population": 12,
+		"lat": 14.60,
+		"lon": 120.98
+	}, {
+		"city": "Tehran",
+		"country": "Iran",
+		"population": 14,
+		"lat": 35.69,
+		"lon": 51.39
+	}, {
+		"city": "Buenos Aires",
+		"country": "Argentina",
+		"population": 14,
+		"lat": -34.60,
+		"lon": -58.38
+	}, {
+		"city": "Rio de Janeiro",
+		"country": "Brazil",
+		"population": 14,
+		"lat": -22.91,
+		"lon": -43.17
+	}, {
+		"city": "Istanbul",
+		"country": "Turkey",
+		"population": 15,
+		"lat": 41.01,
+		"lon": 28.98
+	}, {
+		"city": "Dhaka",
+		"country": "Bangladesh",
+		"population": 18,
+		"lat": 23.81,
+		"lon": 90.41
+	}, {
+		"city": "Los Angeles",
+		"country": "United States",
+		"population": 19,
+		"lat": 34.05,
+		"lon": -118.24
+	}, {
+		"city": "Bejing",
+		"country": "China",
+		"population": 25,
+		"lat": 39.90,
+		"lon": 116.41
+	}, {
+		"city": "Karachi",
+		"country": "Pakistan",
+		"population": 24,
+		"lat": 24.86,
+		"lon": 67.01
 	}
+
 
 
 
